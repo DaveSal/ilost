cd5ce710d16199c98cd1f6b527b8fd319c8846d4
Adis Osmonov <adis.osmonov@gmail.com>
2017-05-13 03:02:22 +0600

    Возможность регистрироваться и логиниться

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  devise_for :users
+  resources :users
+
   root 'home#index'
 
-  get '/lost_articles'  => 'home#lost_articles' 
+  get '/lost_articles'  => 'home#lost_articles'
   get '/found_articles' => 'home#found_articles'
 
   resources :articles
