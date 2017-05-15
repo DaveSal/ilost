2dca8a93ae596e0dc30da2053ac631006a23b033
Adis Osmonov <adis.osmonov@gmail.com>
2017-05-15 14:04:53 +0600

    Пофиксил CRUB в ActiveAdmin

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,6 @@ class Article < ApplicationRecord
   has_attached_file :image,
                     styles: { medium: '300x300>', thumb: '150x150>'},
                     default_url: '/images/:style/missing.png'
-  validates_attachment_content_type :image, 
+  validates_attachment_content_type :image,
                     content_type: ['image/jpeg', 'image/gif', 'image/png']
 end
