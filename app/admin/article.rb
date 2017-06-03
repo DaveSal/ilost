2035f10b715482ad403ae1a792eb22cdf730dd56
aiperi <aiperiakunova@gmail.com>
2017-06-03 15:10:32 +0600

    dobrye ruki

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Article do
-  permit_params :category_id, :subcategory_id, :title, :description
+  permit_params :category_id, :subcategory_id, :title, :description, :contact
 end
