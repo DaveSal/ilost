a71d763d3643be33e4e12160e94b631f81485229
aiperi <aiperiakunova@gmail.com>
2017-06-05 05:36:44 +0600

    article

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@ class Article < ApplicationRecord
 
   has_attached_file :image,
                     styles: { medium: '300x300>', thumb: '150x150>'},
-                    default_url: '/images/:style/foto.jpg'
+                    default_url: '/images/:style/missing.png'
   validates_attachment_content_type :image,
                     content_type: ['image/jpeg', 'image/gif', 'image/png']
 
